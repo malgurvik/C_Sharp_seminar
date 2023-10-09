17f35964f5117dab270f8f0a391a1914da4d68b7
Malgurvik <malgurvik@gmail.com>
2023-10-09 23:22:54 +0300

    add task10,13

---
+++ task10_принимает_трехзначное_показывает2число/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//int second = number / 10;
+//int result = second % 10;
+int result = number / 10 % 10;
+System.Console.WriteLine(result);
---
+++ task13_показывает3число/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>100 && number<1000)
+{
+    int third = number % 10;
+    System.Console.WriteLine(third);
+}
+
+else if (number>999)
+{
+    while (number>=1000)
+    {
+        number = number / 10; 
+    }
+    int third2 = number % 10;
+    System.Console.WriteLine(third2);
+}
+else if (number<100)
+{
+    System.Console.WriteLine("Третьей цифры нет(");
+}
